fbb02c1ede40a5660d2163d7c9741df93a596fa8
Alexandre Philippon <56894822+alexphil12@users.noreply.github.com>
2022-04-30 22:47:39 +0200

    Update make_chromagramme.m

--- make_chromagramme.m
+++ make_chromagramme.m
@@ -3,7 +3,7 @@ N=length(Signal);
 nbre_frame=floor(N/fe);
 Chromagramme=zeros(12,nbre_frame);
 for j=1:nbre_frame
-    Chromagramme(:,j)=make_chroma(Signal(fe*(j-1)+1:fe*j),fe,ecart);
-end
+    Chromagramme(:,j)=make_chroma(Signal(fe*(j-1)+1:fe*j),fe,ecart);%la fonction make chroma est simplement appliqué à chaque seconde du signal et sauvegardée dans un tableau
+end%chromagramme
 end
 
