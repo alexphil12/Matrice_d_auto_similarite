09c68d33cc598689bfa8b94ff2078ec0105a57c0
Alexandre Philippon <56894822+alexphil12@users.noreply.github.com>
2022-04-30 22:02:00 +0200

    Add files via upload

---
+++ make_chromagramme.m
@@ -0,0 +1,9 @@
+function Chromagramme=make_chromagramme(Signal,fe,ecart)
+N=length(Signal);
+nbre_frame=floor(N/fe);
+Chromagramme=zeros(12,nbre_frame);
+for j=1:nbre_frame
+    Chromagramme(:,j)=make_chroma(Signal(fe*(j-1)+1:fe*j),fe,ecart);
+end
+end
+
