6d4efb6033b4e17700a9c5a27adcfc77c52e5199
Alexandre Philippon <56894822+alexphil12@users.noreply.github.com>
2022-05-01 16:09:23 +0200

    Add files via upload

--- make_filtre_triangu.m
+++ make_filtre_triangu.m
@@ -1,6 +1,6 @@
 function filtre=make_filtre_triangu(f_m_minus,f_m,f_m_plus,fe)
 h=f_m_minus;
-filtre=zeros(1,fe/2);
+filtre=zeros(1,floor(fe/2));
 if(f_m_minus~=f_m &&f_m~=f_m_plus)
     for k=f_m_minus:f_m
         filtre(h)=-(k-f_m_minus)/(f_m-f_m_plus);
@@ -10,8 +10,7 @@ if(f_m_minus~=f_m &&f_m~=f_m_plus)
         filtre(h)=(f_m_plus-d)/(f_m_plus-f_m);
         h=h+1;
     end
-end
-if(f_m_minus==f_m && f_m==f_m_plus)
-    filtre(h)=1;
+else
+    filtre(f_m)=1;
 end
 end
